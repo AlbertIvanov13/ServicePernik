4732f131803cfbb0d3d38bced672a11f145b6d79
AlbertIvanov13 <albertivanov187@gmail.com>
2022-03-29 17:56:12 +0300

    UploadProject

--- ServicePernik/ServicePernik/Controllers/HomeController.cs
+++ ServicePernik/ServicePernik/Controllers/HomeController.cs
@@ -28,6 +28,15 @@ namespace ServicePernik.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
